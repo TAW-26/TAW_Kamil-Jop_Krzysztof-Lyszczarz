e366ec5e0f476556db90714d4026e20b4ce319cb
Krzysztof Łyszczarz <klyszczarz03@gmail.com>
2026-04-26 11:52:35 +0200

    dodanie brakujących widoków stron(polityka prywatności, kontakt, regulamin, ustawienia profilu, przypominanie hasła)

--- frontend/src/app/pages/forgot-password/forgot-password.ts
+++ frontend/src/app/pages/forgot-password/forgot-password.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { AuthInput } from '../../shared/components/auth-input/auth-input';
+import { Button } from '../../shared/components/button/button';
 import { Footer } from '../../shared/components/footer/footer';
 import { Navbar } from '../../shared/components/navbar/navbar';
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [Navbar, Footer],
+  imports: [Navbar, Footer, AuthInput, Button, RouterLink],
   templateUrl: './forgot-password.html',
   styleUrl: './forgot-password.css',
 })
